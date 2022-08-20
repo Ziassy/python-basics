0dd4d89ed4b2c7ee442fc8bd508986a91836ed30
pauziah <pauziah@enablr.id>
2022-08-20 19:22:23 +0700

    Create Object class

--- OopDemo.py
+++ OopDemo.py
@@ -2,3 +2,14 @@
 #sum, multipication, addition, constant
 #in class we have methods, variabel, instance variables, constructor etc
 # Objects for your class
+
+class Calculator:
+    num = 100
+
+    def getData(self):
+        print("I'm now executing as method in class")
+
+# create object
+obj = Calculator() #syntax to create objects in python
+obj.getData() #call method in class
+print(obj.num)
