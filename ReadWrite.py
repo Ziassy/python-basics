d9ecdf08ce07ec3107722b2a4fa62c17c82379a8
pauziah <pauziah@enablr.id>
2022-08-20 20:14:51 +0700

    Adding readLine()

--- ReadWrite.py
+++ ReadWrite.py
@@ -1,4 +1,9 @@
 file = open("text.txt")
 # read all the contents of file
 # Read n number of characters by passing parameters
-print(file.read(5)) #output abc d
+# print(file.read(5)) #output abc d
+
+# read one single line at a time readLine()
+print(file.readline(2)) #Output line line 2 > ab
+
+file.close()
