8c24141131dbab78620d7379029fd2d7df8bc3cb
pauziah <pauziah@enablr.id>
2022-08-20 19:13:20 +0700

    First Commit

---
+++ OopDemo.py
@@ -0,0 +1,4 @@
+#classes are user defined blueprint or prototype
+#sum, multipication, addition, constant
+#in class we have methods, variabel, instance variables, constructor etc
+# Objects for your class
