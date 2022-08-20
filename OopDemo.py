76beab3cf6f80326763913c3303be36ce25b5604
pauziah <pauziah@enablr.id>
2022-08-20 19:41:40 +0700

    Create instance variabel and adding parameter constructor

--- OopDemo.py
+++ OopDemo.py
@@ -3,17 +3,31 @@
 #in class we have methods, variabel, instance variables, constructor etc
 # Objects for your class
 
+# self keyword is mandatory for calling variables names into method
+#intance and class variabel have whole different purpose
+# constructor name should be __init__
+#new keyword is not required when you create object
+
 class Calculator:
-    num = 100
+    num = 100 #class variabel
 
     # init is keyword to declare constructor
-    def __init__(self):
+    def __init__(self, a, b):
+        # create instance variabel
+        self.firstNumber = a
+        self.secondNumber = b
         print("I am called automatically when object is created")
 
     def getData(self):
         print("I'm now executing as method in class")
 
+    def Summation(self):
+        sum = self.firstNumber + self.secondNumber + self.num
+        return sum
+
 # create object
-obj = Calculator() #syntax to create objects in python
+# adding parameters constructor using instance variable
+obj = Calculator(2, 5) #syntax to create objects in python
 obj.getData() #call method in class
 print(obj.num)
+print(obj.Summation())
